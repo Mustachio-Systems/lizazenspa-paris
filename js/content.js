f8733c930eefadf7b14be0a43fa607cc06cae557
Luis Pena <lapgonzalez96@gmail.com>
2026-02-02 09:48:26 -0400

    Service prices and information change to closely resemble client provided services

--- js/content.js
+++ js/content.js
@@ -3,25 +3,26 @@ const spaData = {
     contact: {
         phone: "06 03 69 36 49",
         address: "10, rue Auguste Chabrières, 75015 Paris",
-        metro: "Métro 12 (Porte de Versailles), Bus 39/80, Tram T2/T3A",
+        metro: "Métro 12 (Porte de Versailles), Bus 39 et 80 (Hameau), Tram T2, T3A (Desnouettes)",
         whatsapp: "https://wa.me/33603693649"
     },
     prices: {
         thaiOil: [
-            { time: "Massage Thaï Relax (Huile)", price: "65€ / 30min" },
-            { time: "Massage Sportif Fort", price: "70€ / 30min" },
-            { time: "Thaï Traditionnel (sans huile)", price: "50€ / 30min" },
-            { time: "Réflexologie Plantaire", price: "50€ / 30min" }
+            // Title in HTML will be "Thai Massage fort / doux"
+            { time: "30 minutes", price: "70€" },
+            { time: "1 heure", price: "100€" },
+            { time: "1h30", price: "150€" },
+            { time: "2 heures (avec soin visage)", price: "195€" }
         ],
         ventouse: [
-            { time: "Ventouses (*) + Massage", price: "150€ / 1h" },
-            { time: "Shiatsu Deep Tissue", price: "55€ / 30min" },
-            { time: "Massage Crânien", price: "50€ / 30min" }
+            // Title in HTML will be "Ventouse Thérapie"
+            { time: "1h00", price: "150€" },
+            { time: "1h30", price: "190€" },
+            { time: "2h00", price: "250€" }
         ],
         hotStone: [
-            { time: "Massage Pierres Chaudes", price: "120€ / 1h" },
-            { time: "Soin du Visage (Facial)", price: "65€ / 30min" },
-            { time: "Massage Duo (Soin Visage incl.)", price: "180€ / 1h" }
+             // Title in HTML will be "Pierres Chaudes"
+            { time: "1h10", price: "150€" }
         ]
     }
 };
